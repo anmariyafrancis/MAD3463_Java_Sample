61fa056dc7d7a29ca14ea9c8e1f31e0471c562d0
anmariya <anmariya1915@gmail.com>
2020-01-27 21:26:49 -0500

    Day5functionOverLoading

---
+++ src/main/java/com/lambton/Arithmetic.java
@@ -0,0 +1,39 @@
+package com.lambton;
+
+public class Arithmetic {
+
+    public int add(int a,int b){
+        return a+b;
+    }
+    public float add(float a,float b){
+        return a+b;
+    }
+    public String add(String a, String b){
+        return a+b;
+    }
+    public String add(int a,String b){
+        return a+b;
+    }
+    public int add(int a,int b,int c){
+        return a+b+c;
+    }
+    public float add(int a,int b,float c){
+        return (float)a+b+c;
+    }
+    public float add(float a,int b){
+        return a+b;
+    }
+    public float add(float a,int b,int c){
+        return a+b+c;
+    }
+    public String add(String a,int b,Float c){
+        return a+b+c;
+    }
+    double add(int a,double b){
+        return a+b;
+    }
+    float add(int a,float b){
+        return a+b;
+    }
+
+}
--- src/main/java/com/lambton/StudentMainClass.java
+++ src/main/java/com/lambton/StudentMainClass.java
@@ -10,7 +10,7 @@ public class StudentMainClass
 {
     public static void  main(String[] arg)
     {
-        Student[] listOfStudents=new Student[2];
+        /*Student[] listOfStudents=new Student[2];
 
         LocalDate s1BirthDate=LocalDate.of(1996,Month.APRIL,12);
         Student s1=new Student(1, "Anmariya","Francis",s1BirthDate,Gender.FEMALE,85,75,65,80,70);
@@ -19,14 +19,14 @@ public class StudentMainClass
         s1.calculateResult();
         s1.printData();*/
 
-        LocalDate s2BirthDate=LocalDate.of(1995,Month.APRIL,6);
+        /*LocalDate s2BirthDate=LocalDate.of(1995,Month.APRIL,6);
         Student s2=new Student(2, "Aldin","Joseph",s2BirthDate,Gender.FEMALE,65,55,46,71,92);
         /*s2.calculateTotalMarks();
         s2.calculatePercentage();
         s2.calculateResult();
         s2.printData();*/
 
-        listOfStudents[0]=s1;
+        /*listOfStudents[0]=s1;
         listOfStudents[1]=s2;
 
         for(Student s:listOfStudents){
@@ -34,7 +34,8 @@ public class StudentMainClass
             s.calculatePercentage();
             s.calculateResult();
             s.printData();
-        }
-
+        }*/
+        Arithmetic arithmetic=new Arithmetic();
+        System.out.println(arithmetic.add(10,20));
     }
 }
